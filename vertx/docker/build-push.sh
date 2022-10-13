70d04318756381ea548e2dce878950684f7993ef
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-10-13 18:51:08 +0530

    update docker build push script

--- vertx/docker/build-push.sh
+++ vertx/docker/build-push.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 MAJOR_RELEASE=4
-MINOR_RELEASE=0
+MINOR_RELEASE=5
+PATCH_RELEASE=alpha
 COMMIT_ID=`git log -1 --pretty=%h` # last commit-id in short form
 
 # To be executed from project root
-docker build -t ghcr.io/datakaveri/lip-depl:$MAJOR_RELEASE.$MINOR_RELEASE-alpha-$COMMIT_ID -f docker/depl.dockerfile . && \
-docker push ghcr.io/datakaveri/lip-depl:$MAJOR_RELEASE.$MINOR_RELEASE-alpha-$COMMIT_ID
+docker build -t ghcr.io/datakaveri/lip-depl:$MAJOR_RELEASE.$MINOR_RELEASE.$PATCH_RELEASE-$COMMIT_ID -f docker/depl.dockerfile . && \
+docker push ghcr.io/datakaveri/lip-depl:$MAJOR_RELEASE.$MINOR_RELEASE.$PATCH_RELEASE-$COMMIT_ID
 
-docker build -t ghcr.io/datakaveri/lip-dev:$MAJOR_RELEASE.$MINOR_RELEASE-alpha-$COMMIT_ID -f docker/dev.dockerfile . && \
-docker push ghcr.io/datakaveri/lip-dev:$MAJOR_RELEASE.$MINOR_RELEASE-alpha-$COMMIT_ID
+docker build -t ghcr.io/datakaveri/lip-dev:$MAJOR_RELEASE.$MINOR_RELEASE.$PATCH_RELEASE-$COMMIT_ID -f docker/dev.dockerfile . && \
+docker push ghcr.io/datakaveri/lip-dev:$MAJOR_RELEASE.$MINOR_RELEASE.$PATCH_RELEASE-$COMMIT_ID
