7fff17a3a941f3f169492513c38d75bf6df50651
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2023-04-05 12:25:30 +0530

    update image tag to 5.0.0-alpha

--- vertx/docker/build-push.sh
+++ vertx/docker/build-push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MAJOR_RELEASE=4
-MINOR_RELEASE=5
-PATCH_RELEASE=alpha
+MAJOR_RELEASE=5
+MINOR_RELEASE=0
+PATCH_RELEASE=0-alpha
 COMMIT_ID=`git log -1 --pretty=%h` # last commit-id in short form
 
 # To be executed from project root
